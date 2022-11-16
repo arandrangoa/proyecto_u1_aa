3ca9d00e7beee6739ae9d3c22a2aeda5cd9076a8
Bryan Mullo <bfmullo@uce.edu.ec>
2022-11-15 19:39:21 -0500

    Gestion de dependencias tradicional Taller 4

---
+++ src/main/java/com/example/demo/ProyectoU1BmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU1BmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU1BmApplication.class, args);
+	}
+
+}
